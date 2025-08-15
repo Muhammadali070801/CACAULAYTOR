60603a623f9e2a8594f2f36cc179c90afb98a9a6
Muhammadali070801 <hdsfhjgchfdtyhvb@gmail.com>
2025-08-15 19:14:09 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+// alert("Hush kelibsiz ")
+
+
+// let name = prompt("Qaysi Filimni tomosha qilmoqchsiz");
+// let age = prompt("Qaysi qatordan joy kerak ");
+
+// alert(`Siz ${name} filmga ${age} joyni band qildingiz`)
+// alert("Kirishingiz Mumkin")
+
+// console.log(`Siz ${name} filmga ${age} joyga band qildingiz `)
+// console.log("Kirishingiz Mumkin")
+
+
+
+let ism = prompt("Iltmos ismingizni kiriting");
+let familya = prompt("Iltmos familyanginzi kiriting ");
+let yosh = prompt("Iltmos yoshingini kiriting ");
+let email = prompt("Iltmos emailgini kiriting ");
+let parol = prompt("Iltmos parolni kirinting kiriting ");
+
+
+alert(`Sizni ismingiz ${ism} `);
+alert(`Sizni famailyangiz ${familya} `);
+alert(`Sizni yoshingiz ${yosh} `);
+alert(`Sizni emailingiz ${email}@gmail.com `);
+alert(`Sizni parolingiz ${parol}  `);
+alert(`Sizni google uchun email ochingiz ${email}@gmail.com`);
+
+
+
+// let a = 12=1
+// alert(a)
+
+// alert(1 / 0);
+
+// let a = 9485474857474n;
+// alert(a)
+
+
